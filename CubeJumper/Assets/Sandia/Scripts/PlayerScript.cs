3c2efc74337e303fe99b7b43c3edacdcd8069647
OrdenadorInsti02 <aranroig@roig.com>
2019-02-07 14:32:50 +0100

    Xabi eres gai

---
+++ CubeJumper/Assets/Sandia/Scripts/PlayerScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(0.001f, 0, 0));
+        }
+    }
+}
