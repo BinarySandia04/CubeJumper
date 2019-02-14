66ccc792886b2cbf26c76ebb4be98164b83ced3a
nepe <gay@lol>
2019-02-14 14:33:18 +0100

    Hola

--- CubeJumper/Assets/Sandia/Scripts/PlayerScript.cs
+++ CubeJumper/Assets/Sandia/Scripts/PlayerScript.cs
@@ -2,12 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerScript : MonoBehaviour {
-	void Update ()
+
+    [Header("Key binds")]
+    public KeyCode forward;
+    public KeyCode back;
+    public KeyCode left;
+    public KeyCode right;
+    [Space(20)]
+    public float forwardSpeed;
+    public float lateralSpeed;
+    public Vector3 maxSpeed = new Vector3();
+    [Space]
+    public float deaccelerationFloat;
+    Rigidbody rb;
+    Vector3 currentForce = new Vector3();
+    void Awake()
     {
-        if (Input.GetKey(KeyCode.A))
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate ()
+    {
+        if (GetKey(forward) && currentForce.x > maxSpeed.x * -1)
         {
-            transform.Translate(new Vector3(0.001f, 0, 0));
+            currentForce.x -= forwardSpeed;
         }
+        if (GetKey(back) && currentForce.x < maxSpeed.x)
+        {
+            currentForce.x += forwardSpeed;
+        }
+        if (GetKey(left) && currentForce.z > maxSpeed.z * -1)
+        {
+            currentForce.z -= lateralSpeed;
+        }
+        if (GetKey(right) && currentForce.z < maxSpeed.z)
+        {
+            currentForce.z += lateralSpeed;
+        }
+        Deaccelerate();
+        rb.velocity = currentForce;
+    }
+
+    private void Deaccelerate()
+    {
+        Vector3 quiet = new Vector3(); // 0,0,0
+        float finalX = Mathf.Lerp(currentForce.x, quiet.x, deaccelerationFloat);
+        float finalZ = Mathf.Lerp(currentForce.z, quiet.z, deaccelerationFloat);
+        currentForce.x = finalX;
+        currentForce.z = finalZ;
+    }
+
+    private bool GetKey(KeyCode k)
+    {
+        if (Input.GetKey(k)) return true;
+        else return false;
     }
 }
